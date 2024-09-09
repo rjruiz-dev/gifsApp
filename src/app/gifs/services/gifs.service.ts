9f37e959c199bbefa7047940fd4d760e69ce294c
rjruiz <rjruizsf@gmail.com>
2024-09-09 15:24:08 -0300

    Validaciones al servicio

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -6,6 +6,7 @@ import { Injectable } from '@angular/core';
 export class GifsService {
 
   private _tagsHistory: string[] = [];
+  private apiKey: string = 'Yd66AqPNPO2RKlTebQoc2PC75r0xVKYf';
 
   constructor() { }
 
@@ -13,9 +14,30 @@ export class GifsService {
     return [...this._tagsHistory];
   }
 
+  private organizeHistory(tag: string) {
+    // paso el tag a minuscula
+    tag = tag.toLowerCase();
+
+    // si _tagsHistory incluye el tag nuevo que recibo como argumento, lo elimino
+    if(this._tagsHistory.includes(tag)){
+      
+      // obtengo un arreglo con con todos los tag menos el que coincida con el que ya tenia
+      this._tagsHistory = this._tagsHistory.filter( (oldTag) => oldTag !== tag )
+    }
+
+    // inserto el nuevo elemento al inicio
+    this._tagsHistory.unshift( tag );
+
+    // limito el arreglo a 10 elementos
+    this._tagsHistory = this.tagsHistory.splice(0,10);
+  }
+
   // Almacena la busqueda
   searchTag(tag: string): void {
-    this._tagsHistory.unshift(tag);
+    if (tag.length === 0) return;
+
+    this.organizeHistory(tag);   
+    
     console.log(this._tagsHistory);
   }
 }
