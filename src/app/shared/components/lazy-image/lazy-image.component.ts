442123b5ffdfbe923e0b271fdd9d2dc97c5580a6
rjruiz <rjruizsf@gmail.com>
2024-09-11 12:26:56 -0300

    Lazy Image - parte 2

--- src/app/shared/components/lazy-image/lazy-image.component.ts
+++ src/app/shared/components/lazy-image/lazy-image.component.ts
@@ -1,9 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'shared-lazy-image',
   templateUrl: './lazy-image.component.html'
 })
-export class LazyImageComponent {
+
+/**
+  Al estar aquí, nosotros deberíamos de recibir forzadamente el URL el URL siempre lo vamos a necesitar.
+  El URL de la imagen que queremos mostrar y la tiene que mandar desde el padre.
+*/
+
+export class LazyImageComponent implements OnInit {
+ 
+/**
+  El URL lo vamos a definir con el input y le vamos a poner public URL de tipo string.
+  Se podria igualarla a un string vacío, pero no sería el caso porque siempre lo ocupamos.
+  Entonces  lo definimos asi url!, siempre lo tiene que mandar.
+*/
+  @Input()
+  public url!: string;
+
+  @Input()
+  public alt: string = '';
+
+  // cuando se carga por primera vez, esto no ha cargado
+  public hasLoaded: boolean = false;
+
+  ngOnInit(): void {
+    if(!this.url) throw new Error('URL property is required.');
+  }
+
+  onLoad() {
+    console.log('Image loaded');
+    // cuando se carga la imagen 
+    this.hasLoaded = true;
+  }
 
 }
